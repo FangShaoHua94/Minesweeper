4e03a32c15de4c3e73a890bbb685930816a7b229
Daythehangedman <59592689+Daythehangedman@users.noreply.github.com>
2020-02-29 21:00:44 +0800

    singleLayerButton

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,5 +1,6 @@
 
 import javafx.fxml.FXML;
+import javafx.geometry.Bounds;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
@@ -27,5 +28,17 @@ public class MainWindow extends AnchorPane {
         smiley.setStyle("-fx-background-color: red");
     }
 
+    @FXML
+    public void initialize(){
+        int row=board.getRowCount();
+        int col=board.getColumnCount();
+
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                Bounds b=board.getCellBounds(1,1);
+                board.add(new Tile(i,j),i,j);
+            }
+        }
+    }
 
 }
--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -1,2 +1,52 @@
-public class Tile {
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class Tile extends Button {
+
+    private final int rowIndex;
+    private final int colIndex;
+    private boolean isMine=false;
+    private boolean isFlag=false;
+    private boolean isReveal=false;
+
+    public Tile(int rowIndex,int colIndex){
+        this.colIndex=colIndex;
+        this.rowIndex=rowIndex;
+        setTile();
+
+    }
+
+    private void setTile(){
+        setPrefSize(100,30);
+        addEventFilter(MouseEvent.MOUSE_PRESSED, e -> {
+            if(!isReveal) {
+                if (e.isPrimaryButtonDown()) {
+//                    reveal();
+//                    setStyle("-fx-background-color: white");
+                    setVisible(false);
+                } else if (e.isSecondaryButtonDown()) {
+                    if(!isFlag){
+                        isFlag=true;
+                    }else{
+                        isFlag=false;
+                    }
+                }
+            }
+        });
+    }
+
+    public boolean checkIsFlag(){
+        return isFlag;
+    }
+
+    public void setMine(){
+        isMine=true;
+    }
+
+    public void reveal(int neighbouringMineCount){
+
+        isReveal=true;
+    }
+
+
 }
